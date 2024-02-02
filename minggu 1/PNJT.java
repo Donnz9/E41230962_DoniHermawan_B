decf95883459cb097abc4a323bfdf88889353626
Donnz9 <158550378+Donnz9@users.noreply.github.com>
2024-02-02 15:01:14 +0700

    Create PNJT.java

---
+++ minggu 1/PNJT.java
@@ -0,0 +1,25 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package pnjt;
+
+import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import androidx.appcompat.app.AppCompatActivity;
+
+/**
+ *
+ * @author donih
+ */
+public class PNJT {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+    }
+    
+}
